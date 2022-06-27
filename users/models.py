4d4466804295393a427950e35bdd7b660ce7eb97
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-27 19:06:11 -0300

    feat(users): create User model inheting AbstractUser and adding is_seller field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.forms import BooleanField
+from .utils import CustomUserManager
+from django.utils import timezone
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(unique=False, null=True, max_length=255)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    updated_at = models.DateTimeField(default=timezone.now)
+    is_seller = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'is_seller']
+    objects = CustomUserManager()
