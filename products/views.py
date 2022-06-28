f950ba36239cc489d6c0d2e733adaf426329e291
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-27 22:17:54 -0300

    feat(products): add funcionality to views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer, GetProductSerializer
+from .models import Product
+from utils.mixins import SerializerByMethodMixin
+from rest_framework import generics
+from .permissions import ProductDetailPermission, ProductPermission
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+class ProductView(SerializerByMethodMixin, generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [ProductPermission]
+    queryset = Product.objects.all()
+    serializer_map = {
+        'GET': GetProductSerializer,
+        'POST': ProductSerializer,
+    }
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ProductDetailView(SerializerByMethodMixin, generics.RetrieveUpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [ProductDetailPermission]
+    queryset = Product.objects.all()
+    serializer_map = {
+        'GET': GetProductSerializer,
+        'PATCH': ProductSerializer,
+    }
+
