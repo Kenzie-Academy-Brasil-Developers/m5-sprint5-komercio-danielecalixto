a697a2fb6c74926c26d4b946b57cefc1d85186aa
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-30 19:44:34 -0300

    fix(products): fix Product Detail Permission using the new seller attribute name instead of user

--- products/permissions.py
+++ products/permissions.py
@@ -14,4 +14,4 @@ class ProductDetailPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == request.user
+        return obj.seller == request.user
