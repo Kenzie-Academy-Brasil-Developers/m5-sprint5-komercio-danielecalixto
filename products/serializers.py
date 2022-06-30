e9adc32b075946cb4d3d6f2e7f380e4e2b46cdd1
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-30 19:45:17 -0300

    fix(products): change serializers using the new seller attribute name instead of user

--- products/serializers.py
+++ products/serializers.py
@@ -3,38 +3,26 @@ from rest_framework import serializers
 from .models import Product
 from users.models import User
 
+class SellerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_seller', 'date_joined']
+
 class ProductSerializer(serializers.ModelSerializer):
+    seller = SellerSerializer(read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'description', 'price', 'quantity', 'is_active']
-        read_only_fields = ['seller']
+        fields = ['id', 'description', 'price', 'quantity', 'is_active', 'seller']
         extra_kwargs = {'is_active': {'required': False}}
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        user = User.objects.get(id=instance.user_id)
-        seller = {}
-        seller['id'] = user.id
-        seller['email'] = user.email
-        seller['first_name'] = user.first_name
-        seller['last_name'] = user.last_name
-        seller['is_seller'] = user.is_seller
-        seller['date_joined'] = user.date_joined
-        representation['seller'] = seller
-        return representation
-
     def create(self, validated_data):
-        user = validated_data.pop('user')
-        product = Product.objects.create(user=user, **validated_data)
+        seller = validated_data.pop('seller')
+        product = Product.objects.create(seller=seller, **validated_data)
         return product
 
 class GetProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['description', 'price', 'quantity', 'is_active']
+        fields = ['description', 'price', 'quantity', 'is_active', 'seller_id']
         read_only_fields = ['seller_id']
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['seller_id'] = instance.user_id
-        return representation
