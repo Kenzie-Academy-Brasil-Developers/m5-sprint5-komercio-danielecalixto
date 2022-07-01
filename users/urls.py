1615f1c2f62cfee06e3932f03cf32de2fb6e4f12
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-30 22:17:17 -0300

    feat(users): create update and deactivate urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
-from .views import UserView, LoginView, ListNumUsersView
+from .views import UserView, LoginView, ListNumUsersView, UpdateUserView, DeactivateView
 
 urlpatterns = [
     path("accounts/", UserView.as_view()),
-    path("login/", LoginView.as_view()),
-    path("accounts/newest/<int:num>/", ListNumUsersView.as_view())
+    path("accounts/<pk>/", UpdateUserView.as_view()),
+    path("accounts/<pk>/management/", DeactivateView.as_view()),
+    path("accounts/newest/<int:num>/", ListNumUsersView.as_view()),
+    path("login/", LoginView.as_view())
 ]
