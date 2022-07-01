8380eb93047845b96f4a9aff68e5293201291be1
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-30 22:16:32 -0300

    fix(users): add is_active to UserSerializer and create DeactivateSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,9 +5,10 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password', 'first_name', 'last_name', 'is_seller', 'date_joined']
+        fields = ['email', 'password', 'first_name', 'last_name', 'is_seller', 'date_joined', 'is_active']
         extra_kwargs = {
             'is_seller': {'required': True},
+            'is_active': {'required': False, 'write_only': True},
             'password': {'write_only': True},
             'date_joined': {'read_only': True},
             'email': {'validators': [UniqueValidator(queryset=User.objects.all(), message='This email already exists.')]}
@@ -16,6 +17,13 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class DeactivateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_seller', 'date_joined', 'is_active']
+        read_only_fields = ['id', 'email', 'first_name', 'last_name', 'is_seller', 'date_joined']
+
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField(write_only=True)
